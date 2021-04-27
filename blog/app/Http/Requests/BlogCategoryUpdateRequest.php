58e1fa07615dfaa0eb5d197955a6330f174204ff
Rozi25 <zoriana.kyrisha@oa.edu.ua>
2021-04-27 14:56:45 +0300

    lab8

--- blog/app/Http/Requests/BlogCategoryUpdateRequest.php
+++ blog/app/Http/Requests/BlogCategoryUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class BlogCategoryUpdateRequest extends FormRequest
+class BlogPostUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,6 +13,7 @@ class BlogCategoryUpdateRequest extends FormRequest
      */
     public function authorize()
     {
+        //return->auth()->check();
         return true;
     }
 
@@ -26,8 +27,9 @@ class BlogCategoryUpdateRequest extends FormRequest
         return [
             'title' => 'required|min:5|max:200',
             'slug' => 'max:200',
-            'description' => 'string|max:500|min:3',
-            'parent_id' => 'required|integer|exists:blog_categories,id',
+            'excerpt' => 'max:500',
+            'content_raw' => 'required|string|min:5|max:10000',
+            'category_id' => 'required|integer|exists:blog_categories,id',
         ];
     }
 }
