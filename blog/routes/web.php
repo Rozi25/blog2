99863dde3892846d2029f9542143788229aae630
Rozi25 <zoriana.kyrisha@oa.edu.ua>
2021-05-13 00:06:06 +0300

    lab13

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,8 +1,7 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RestTestController;
 use App\Http\Controllers\DiggingDeeperController;
+use App\Http\Controllers\RestTestController;
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,45 +12,43 @@ use App\Http\Controllers\DiggingDeeperController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+Route::resource('rest', RestTestController::class)->names('restTest');
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
 //Адмінка
 $groupData = [
     'namespace' => 'App\Http\Controllers\Blog\Admin',
     'prefix' => 'admin/blog',
 ];
-
-Route::group(['prefix' => 'digging_deeper'], function () {
-
-    Route::get('collections', [DiggingDeeperController::class, 'collections'])
-
-        ->name('digging_deeper.collections');
-
-});
 Route::group($groupData, function () {
     //BlogCategory
-
-
     $methods = ['index','edit','store','update','create',];
     Route::resource('categories', CategoryController::class)
         ->only($methods)
         ->names('blog.admin.categories');
-
     //BlogPost
     Route::resource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
 });
+Route::group(['prefix' => 'digging_deeper'], function () {
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+        ->name('digging_deeper.collections');
 
-Route::resource('rest', RestTestController::class)->names('restTest');
+    Route::get('process-video',  [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
 
-Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
-    Route::resource('posts', PostController::class)->names('blog.posts');
+    Route::get('prepare-catalog',  [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
+
 });
